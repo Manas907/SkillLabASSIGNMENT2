2b94fa9435f96699df225f61683e74aeeac75025
Manas907 <127177654+Manas907@users.noreply.github.com>
2024-02-25 23:41:00 +0530

    Create cachingService.js

---
+++ cachingService.js
@@ -0,0 +1,7 @@
+exports.cacheData = async (key, data) => {
+    // Implementation logic for in-memory caching
+};
+
+exports.getCachedData = async (key) => {
+    // Implementation logic to retrieve cached data
+};
