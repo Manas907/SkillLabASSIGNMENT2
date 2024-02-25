0d8ddc708a8a7ad5679e3ceda3ac2ceba9b4d0c4
Manas907 <127177654+Manas907@users.noreply.github.com>
2024-02-25 23:42:46 +0530

    Create userController.js

---
+++ userController.js
@@ -0,0 +1,17 @@
+const User = require('../models/user');
+
+exports.registerUser = async (req, res) => {
+    // Implementation logic to register a new user
+};
+
+exports.loginUser = async (req, res) => {
+    // Implementation logic to log in a user
+};
+
+exports.getUserById = async (req, res) => {
+    // Implementation logic to retrieve a user by ID
+};
+
+exports.deleteUser = async (req, res) => {
+    // Implementation logic to delete a user
+};
